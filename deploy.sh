a26fcff9b944a48bfced4b8b9668fe0445995535
Oladipupo <oladipupo@sproutlyhq.com>
2024-10-14 05:48:37 -0500

    add pipeline script

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+APP_DIR="~/fable"
+BACKEND_DIR="$APP_DIR/fable-v3-api/"
+FRONTEND_DIR="/$APP_DIR/fable-v3-frontend/"
+BRANCH="devops"
+
+echo "update API code base"
+
+cd $BACKEND_DIR || exit
+
+echo "Pulling the latest code from branch $BRANCH..."
+git fetch --all
+git reset --hard origin/$BRANCH
+
+echo "refreshing deploymemnt..."
+cd $APP_DIR
+
+echo "Stopping the current Docker Compose services..."
+docker-compose down
+
+# Rebuild and start the Docker Compose services
+echo "Rebuilding and starting Docker Compose services..."
+docker-compose up -d --build
+
+echo "waiting for health status"
+sleep 30
+
+echo "Checking Docker services status..."
+docker ps -a
+
+echo "Deployment completed successfully."
