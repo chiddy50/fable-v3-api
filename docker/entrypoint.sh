cac457c93d38e86ccf2e6dc561f5f0b68b22b035
Ibeun Oladipupo Joseph <oladipupo.ibeun@gmail.com>
2024-10-23 05:40:35 -0500

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,15 +2,25 @@
 set -e
 
 # Run database migrations
-echo "Running prisma generate..."
-#npx prisma migrate reset --force
+echo "Running db reset..."
+npx prisma migrate reset --force  --skip-seed
+echo "Db reset complete ola"
+
+echo "running prisma generate"
 npx prisma generate
-#npx prisma db push
+echo "running prisma generate complete ola"
+
+echo "running db push"
+npx prisma db push
+echo "db push complete"
+
+echo "running db seed"
+npx prisma db seed
+echo "db seed complete"
 
 # Seed the database
 echo "build API artifacts..."
 npm run build
 
-# Start the application
-echo "Starting the application..."
+#Start the application
 npm start
