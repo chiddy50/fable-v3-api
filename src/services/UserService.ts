46b8e0750346ed63ee3e8cf8f71db0f11c0651a4
Chidi Michael <43938389+chiddy50@users.noreply.github.com>
2025-01-09 22:33:23 +0100

    User can now see transactions clearer

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -140,6 +140,7 @@ export class UserService implements IUserService {
               // storyAccess: true
             }
           },
+          articles: true,
           transactions: true,
           socialMedia: true
         }
